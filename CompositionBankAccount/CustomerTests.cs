dc1acfcd43aacf9a5ef34dae7fc03a11d5515f11
mort358d <mort358d@CV-BB-5860>
2019-08-15 11:00:53 +0200

    Wrote customer tests

--- CompositionBankAccount/CustomerTests.cs
+++ CompositionBankAccount/CustomerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CompositionBankAccount.Entities;
 
@@ -10,49 +11,139 @@ namespace CompositionBankAccount.EntitiesTest
         [TestMethod]
         public void GetDebts()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(50.4m),
+                new Account(12m),
+                new Account(-100.55m),
+                new Account(-24.8m),
+                new Account(0m)
+            });
+
+            Assert.AreEqual(-100.55m + -24.8m, testCustomer.GetDebts());
         }
 
         [TestMethod]
         public void GetAssets()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(50.4m),
+                new Account(12m),
+                new Account(-100.55m),
+                new Account(-24.8m),
+                new Account(0m)
+            });
+
+            Assert.AreEqual(50.4m + 12m, testCustomer.GetAssets());
         }
 
         [TestMethod]
         public void GetTotalBalance()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(50.4m),
+                new Account(12m),
+                new Account(-100.55m),
+                new Account(-24.8m),
+                new Account(0m)
+            });
+
+            Assert.AreEqual(testCustomer.GetAssets() + testCustomer.GetDebts(), testCustomer.GetAssets());
         }
 
         [TestMethod]
         public void GetRatingTest1()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-2000000),
+                new Account(-500001),
+                new Account(1000000),
+                new Account(250001)
+            });
+            Assert.AreEqual(1, testCustomer.Rating);
         }
 
         [TestMethod]
         public void GetRatingTest2()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-2000000),
+                new Account(-500001),
+                new Account(25000),
+                new Account(25001)
+            });
+            Assert.AreEqual(2, testCustomer.Rating);
+
+            testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-2000000),
+                new Account(-500001),
+                new Account(1000000),
+                new Account(250000)
+            });
+            Assert.AreEqual(2, testCustomer.Rating);
         }
 
         [TestMethod]
         public void GetRatingTest3()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-20000000),
+                new Account(-5000000),
+                new Account(1000000),
+                new Account(250000)
+            });
+            Assert.AreEqual(3, testCustomer.Rating);
+
+            testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-2000000),
+                new Account(-500000),
+                new Account(25000),
+                new Account(25000)
+            });
+            Assert.AreEqual(3, testCustomer.Rating);
         }
 
         [TestMethod]
         public void GetRatingTest4()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-24998),
+                new Account(-25000),
+                new Account(49999)
+            });
+            Assert.AreEqual(4, testCustomer.Rating);
+
+            testCustomer = new Customer(new List<Account>()
+            {
+                new Account(0.1m)
+            });
+            Assert.AreEqual(4, testCustomer.Rating);
         }
 
         [TestMethod]
         public void GetRatingTest5()
         {
-            Assert.Fail();
+            Customer testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-200000),
+                new Account(-50000),
+                new Account(49999)
+            });
+            Assert.AreEqual(5, testCustomer.Rating);
+
+            testCustomer = new Customer(new List<Account>()
+            {
+                new Account(-0.1m),
+            });
+            Assert.AreEqual(5, testCustomer.Rating);
         }
     }
 }
